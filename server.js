0ac4cb458c97fc26e954bd6c2145b41ac3f36069
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-07-12 15:47:32 +0530

    worker works on its own and server keeps on giving the jobs
    server add the jobs to jobs and in interval checks for the jobs and gives it to the worker
    worker takes the job and performs it and responds as i am done and this is the badge

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+let id = 0;
+let isWorkerFree = true;
+const jobs = []; // { id: id, params: params }
+const badges = {};
 const getWorkerOptions = () => {
   return {
     host: 'localhost',
@@ -11,24 +15,47 @@ const getWorkerOptions = () => {
   };
 };
 
+const delegateToWorker = (id, params) => {
+  badges[id] = 'Processing ...';
+  const options = getWorkerOptions();
+  options.path += id + '/' + params.username;
+  const request = http.request(options, res => {
+    console.log('Got from worker', res.statusCode);
+  });
+  request.end();
+};
+
+setInterval(() => {
+  if (isWorkerFree && jobs.length) {
+    const job = jobs.shift();
+    console.log('Scheduling jon on worker : ', job.id);
+    delegateToWorker(job.id, job.params);
+  }
+}, 1000);
+
 //log request url and method
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
 
+app.get('/status/:id', (req, res) => {
+  res.write(badges[req.params.id]);
+  res.end();
+});
+
+app.post('/completed-job/:id/:badge', (req, res) => {
+  isWorkerFree = true;
+  badges[req.params.id] = req.params.badge;
+  res.end();
+});
+
 app.post('/:username', (req, res) => {
-  const options = getWorkerOptions();
-  options.path += req.params.username; // /userDate/dad-ka-raneesa
-  const request = http.request(options, response => {
-    let data = '';
-    response.on('data', chunk => (data += chunk));
-    response.on('end', () => {
-      res.write(data);
-      res.end();
-    });
-  });
-  request.end();
+  res.send(`id:${id}`);
+  res.end();
+  jobs.push({ id, params: req.params });
+  badges[id] = 'Scheduled ...';
+  id++;
 });
 
 app.listen(8000, () => console.log('listening on 8000...'));
--- worker.js
+++ worker.js
@@ -1,6 +1,15 @@
 const express = require('express');
-const app = express();
+const http = require('http');
 const { processBadgeRequest } = require('./processBadges');
+const app = express();
+
+const getServerOptions = () => {
+  return {
+    host: 'localhost',
+    port: '8000',
+    method: 'post',
+  };
+};
 
 //log request url and method
 app.use((req, res, next) => {
@@ -8,16 +17,18 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/badge/:username', (req, res) => {
+const informWorkerFree = (id, badge) => {
+  const options = getServerOptions();
+  options.path = `/completed-job/${id}/${badge}`;
+  const req = http.request(options, () => {});
+  req.end();
+};
+
+app.post('/badge/:id/:username', (req, res) => {
+  res.end();
   processBadgeRequest(req.params.username)
-    .then(badge => {
-      res.write(badge);
-      res.end();
-    })
-    .catch(err => {
-      res.write(err);
-      res.end();
-    });
+    .then(badge => informWorkerFree(req.params.id, badge))
+    .catch(err => informWorkerFree(req.params.id, err));
 });
 
 app.listen(5000, () => console.log(`listening on ${5000}...`));
