ba45291c2d0c52d0c015b9fecc81f7dbae701633
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-07-14 17:59:51 +0530

    added expiry of 5 min for created job

--- badgeRequests.js
+++ badgeRequests.js
@@ -4,14 +4,20 @@ const createJob = (client, username) => {
     const receivedAt = ['receivedAt', new Date()];
     const usernameField = ['username', username];
     const fields = status.concat(receivedAt, usernameField);
-    client.hmset(username, fields, (err, res) =>
-      resolve({ username, status: status[1], receivedAt: receivedAt[1] })
-    );
+    const job = { username, status: status[1], receivedAt: receivedAt[1] };
+    client.hmset(username, fields, (err, res) => resolve(job));
+  });
+};
+
+const attachExpiry = (client, job) => {
+  return new Promise((resolve, reject) => {
+    client.expire(job.username, 5 * 60);
+    resolve(job);
   });
 };
 
 const addBadgeRequest = (client, username) => {
-  return createJob(client, username);
+  return createJob(client, username).then(job => attachExpiry(client, job));
 };
 
 const completedProcessing = (client, username, badge) => {
