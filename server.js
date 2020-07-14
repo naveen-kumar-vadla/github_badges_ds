70e0c9e566e8961673f4bd4505c50b035b825881
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-07-14 18:14:59 +0530

    implemented caching
    adding the job into the queue when there is no data with the key as username

--- server.js
+++ server.js
@@ -19,14 +19,21 @@ app.get('/status/:username', (req, res) => {
 });
 
 app.post('/:username', (req, res) => {
-  badgeRequests
-    .addBadgeRequest(redisClient, req.params.username)
-    .then(jobToSchedule => {
-      redisClient.lpush('ipQueue', jobToSchedule.username, () => {
-        res.send(jobToSchedule.username);
-        res.end();
-      });
-    });
+  badgeRequests.get(redisClient, req.params.username).then(userData => {
+    if (userData) {
+      res.send(userData.username);
+      res.end();
+    } else {
+      badgeRequests
+        .addBadgeRequest(redisClient, req.params.username)
+        .then(jobToSchedule => {
+          redisClient.lpush('ipQueue', jobToSchedule.username, () => {
+            res.send(jobToSchedule.username);
+            res.end();
+          });
+        });
+    }
+  });
 });
 
 app.listen(8000, () => console.log('listening on 8000...'));
